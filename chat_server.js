e1cc18675261e853caaa9ed983bd0bf775302019
coofly <coofly@gmail.com>
2014-07-15 10:01:30 +0800

    用户登陆聊天室时，会告知其本聊天室的Github地址了

--- chat_server.js
+++ chat_server.js
@@ -27,7 +27,9 @@ io.on('connection', function(_socket){
 	console.log(_socket.id + ': connection');
 	_socket.emit('user_list', nickname_list);
 	_socket.emit('need_nickname');
-	_socket.emit('server_message', '欢迎来到千寻聊天室~');
+	_socket.emit('server_message', '欢迎来到千寻聊天室~<br/>' +
+		'本聊天室源代码<a href="https://github.com/coofly/qx-chat" target="_blank">' +
+		'https://github.com/coofly/qx-chat</a>，欢迎Star！');
 
 	_socket.on('disconnect', function() {
 		console.log(_socket.id + ': disconnect');
